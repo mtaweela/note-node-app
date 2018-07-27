36daf2ba250ae8485e8228067233626f32659057
mtaweela <mohamed.taweela56@gmail.com>
2018-07-27 14:38:09 +0200

    -- add the list, read and remiove functionalities

--- app.js
+++ app.js
@@ -3,17 +3,62 @@ const yargs = require('yargs');
 
 const notes = require('./notes');
 
-var argv = yargs.argv;
+const titleOptions = {
+    descripe: 'Title of note',
+    demand: true,
+    alias: 't'
+}
+
+
+const bodyOptions = {
+    descripe:'Body of note', 
+    demand: true,
+    alias: 'b'
+}
+var argv = yargs
+    .command('add', 'add a new note', {
+        title: titleOptions,
+        body: bodyOptions
+    })
+    .command('list', 'list all notes')
+    .command('read', 'read a note', {
+        title: {
+            descripe: 'Title of note',
+            demand: true,
+            alias: 't'
+        }
+    })
+    .command('remove', 'remove a note', {
+        title: titleOptions
+    })
+    .help()
+    .argv;
 var command = argv._[0];
 
 if (command === 'add') {
-    notes.addNote(argv.title, argv.body);
+    var note = notes.addNote(argv.title, argv.body);
+    if (note) {
+        console.log('note created');
+        notes.logNote(note);
+    } else {
+        console.log('note title taken');
+    }
 } else if (command === 'list') {
-    notes.getAll();
+    var allNotes = notes.getAll();
+    console.log(`Printing ${allNotes.length} note(s).`);
+    allNotes.forEach(note => notes.logNote(note));
 } else if (command === 'read') {
-    notes.getNote(argv.title);
+    var note = notes.getNote(argv.title);
+    if (note) {
+        console.log('note created');
+        notes.logNote(note);
+    } else {
+        console.log('note does not exist');
+    }
 } else if (command === 'remove') {
-    notes.removeNote(argv.title);
+    var noteRemoved = notes.removeNote(argv.title);
+    var message = noteRemoved ? 'Note was removed' : 'Note not found'
+    console.log(message);
 } else {
 
 }
